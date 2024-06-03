6f9f02986e6a43d45abbf640db917588978b17ef
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-06-03 13:06:30 +0100

    still on the crash course

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+function about() {
+  return (
+    <div>
+      <Head>
+        <title>about the page</title>
+      </Head>
+    </div>
+  );
+}
+
+export default about;
